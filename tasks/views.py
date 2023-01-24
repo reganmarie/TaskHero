0f7b61cde4395cd4bbb8cdda3396853a7ed84931
Regan Tewksbury <regantewksbury@gmail.com>
2023-01-23 20:06:11 -0800

    Feature 16 complete

--- tasks/views.py
+++ tasks/views.py
@@ -19,3 +19,12 @@ def create_task(request):
         "form": form,
     }
     return render(request, "tasks/create.html", context)
+
+
+@login_required
+def my_tasks(request):
+    tasks = Task.objects.filter(assignee=request.user)
+    context = {
+        "tasks": tasks,
+    }
+    return render(request, "tasks/detail.html", context)
